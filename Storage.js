960fb767343aa47ce8675a9c83c109016e036898
hubert_r <59674312+hubiop@users.noreply.github.com>
2022-03-19 23:43:09 -0400

    Add files via upload

--- Storage.js
+++ Storage.js
@@ -35,15 +35,25 @@ function ClearAll() {
 }
 //-------------------------------------------------------------------------------------
 function doShowAll() {
-		var key = "";
-		var list = "<tr><th>Item</th><th>#</th><th>weight (lb)</th><th>price ($)</th></tr>\n";
-		var i = 0;
-		for (i = 0; i <= localStorage.length-1; i++) {
-			key = localStorage.key(i);
-			list += "<tr><td>" + key + "</td><td>\n"
-					+ localStorage.getItem(key) + "</td>"+"<td>" + localStorage.getItem(key)*arguments[0] + "</td><td>"
-					+ localStorage.getItem(key)*arguments[1] + "</td></tr>\n";
+	var key = "";
+	var w = 5;
+	var p=0;
+	var list = "<tr><th>Item</th><th>#</th><th>weight (lb)</th><th>price ($)</th></tr>\n";
+	var i = 0;
+	for (i = 0; i <= localStorage.length-1; i++) {
+		key = localStorage.key(i);
+		if (key==Potatoe){
+			p = 3.49;
 		}
-		document.getElementById('list').innerHTML = list;
+		if (key==Carrot){
+			p = 4.99;
+		}
+		if (key==Apple){
+			p = 7.99;			}
+		list += "<tr><td>" + key + "</td><td>\n"
+			+ localStorage.getItem(key) + "</td>"+"<td>" + localStorage.getItem(key)*w + "</td><td>"
+			+ localStorage.getItem(key)*p + "</td></tr>\n";
+	}
+	document.getElementById('list').innerHTML = list;
 	
 }
